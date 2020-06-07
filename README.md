8803eba757f545c51473330e90afa0476ebd7634
Dave Zheng <dzhen418@gmail.com>
2020-06-07 11:50:24 -0700

    fix a few infra things

---
+++ README.md
@@ -0,0 +1,5 @@
+Run this using sudo docker-compose up
+
+After making changes to Dockerfile, run:
+sudo docker-compose build
+sudo docker-compose up
--- web/app.py
+++ web/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_whole():
-   return "We are all tests on this blessed day"
+   return "We are all testd1s on this blessed day"
 
 if __name__ == '__main__':
    app.run(debug=True, host='0.0.0.0')
